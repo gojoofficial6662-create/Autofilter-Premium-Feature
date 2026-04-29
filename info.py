3c0e64d95d74d5ce8c8cf3b5f609552c837042ad
gojoofficial6662-create <gojoofficial6662@gmail.com>
2026-04-30 02:00:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "Gojoofficial6969@cluster0.tb5l339.mongodb.net")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Gojoofficial6969@cluster0.tb5l339.mongodb.net")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'royal_files') # Collection name (default: dreamcinezone_files)
 
